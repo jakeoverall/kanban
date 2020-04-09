075f6c431cb2be13002465547c9d5a0c88021f09
Jake Overall <joverall22@gmail.com>
2020-04-08 20:09:19 -0600

    removes test

--- clientsrc/tests/unit/modal.spec.js
+++ clientsrc/tests/unit/modal.spec.js
@@ -26,24 +26,4 @@ describe('Modal.vue', () => {
     expect(wrapper.vm.$refs.modalDialog).toBeTruthy()
 
   })
-  // it("the modal can be opened when setting prop open", () => {
-  //   let props = {
-  //     open: true
-  //   }
-  //   const wrapper = shallowMount(modal, {
-  //     propsData: {
-  //       ...props
-  //     }
-  //   })
-  //   wrapper.vm.$on("close", () => {
-  //     props.open = false
-  //   });
-
-  //   wrapper.vm.$refs.modalDialog
-  //   expect(wrapper.vm.$refs.modalDialog).toBeTruthy()
-  //   wrapper.vm.$emit("close")
-  //   wrapper.vm.$nextTick(() => {
-  //     expect(wrapper.vm.$refs.modalDialog).toBeFalsy()
-  //   })
-  // })
 })
