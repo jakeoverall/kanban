d54de4af13f3499367d3b8670be1f75a365c5a55
Jake Overall <joverall22@gmail.com>
2020-04-08 18:45:46 -0600

    neato

--- clientsrc/src/main.js
+++ clientsrc/src/main.js
@@ -22,7 +22,7 @@ Vue.use(Auth0Plugin, {
   }
 });
 
-console.log("show the config", vue.config, "this is a test", "this one will fail for sure")
+console.log("show the config", Vue.config, "this is a test", "this one will fail for sure")
 
 
 Vue.config.productionTip = false;
